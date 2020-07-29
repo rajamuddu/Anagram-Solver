5b7d0b1d9e060889315d9c7e1c3ced99ebb079c1
rajamuddu <rajashankar0206@gmail.com>
2020-07-29 17:19:20 +0000

    Modified anagram code

--- app/dict_app.py
+++ app/dict_app.py
@@ -13,6 +13,9 @@ def adder_page():
 		p = re.compile('^[a-zA-Z]+$')
 		get_word=request.form['word']
 		input_word=p.match(get_word)
+		if len(get_word)>10:
+			errors+= "{!r} is too long. Please retry\n".format(request.form['word'])
+			return render_template("error.html", e=errors)
 		if input_word is not None:
 			if dict_check(anagrams(get_word.lower())):
 				return render_template("result.html", result=dict_check(anagrams(get_word.lower())))
